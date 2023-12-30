12dfc5e7ca526e9e88b8ff9e5ed856fd3fcb6004
aldoprogrammer <aldobesma@gmail.com>
2023-12-30 21:01:52 +0700

    project finish

--- client/src/CreateUser.jsx
+++ client/src/CreateUser.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const CreateUser = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [age, setAge] = useState();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:3001/createUser",
+    {name, email, age})
+    .then(result => {
+      console.log(result)
+      navigate('/');
+    })
+    .catch(err => console.log(err))
+
+  }
+
   return (
-    <div>
-      Create
+    <div className='d-flex vh-100 bg-primary
+    justify-content-center align-items-center
+    '>
+      <div className='w-50 bg-white rounded p-3'>
+        <form onSubmit={handleSubmit}>
+          <h2>Add User</h2>
+          <div className='mb-2'>
+            <label>Name</label>
+            <input type='text' placeholder='Enter name'
+              className='form-control'
+              onChange={(e) => setName(e.target.value)}
+              />
+          </div>
+          <div className='mb-2'>
+            <label>Email</label>
+            <input type='email' placeholder='Enter email'
+              className='form-control'
+              onChange={(e) => setEmail(e.target.value)}
+              />
+          </div>
+          <div className='mb-2'>
+            <label>Age</label>
+            <input type='text' placeholder='enter age'
+              className='form-control'
+              onChange={(e) => setAge(e.target.value)}
+              />
+          </div>
+          <button className='btn
+          btn-success'>Submit</button>
+        </form>
+      </div>
     </div>
   )
 }
--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -1,13 +1,28 @@
-import React, { useState } from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const Users = () => {
-    const [users, setUsers] = useState([{
-        Name: 'aldo ls', Email: 'aldobesma@gmail.com', age: '20'
-    }]);
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3001')
+        .then(result => setUsers(result.data))
+        .catch(err => console.log(err))
+    }, [])
+
+    const handleDelete = (id) => {
+        axios.delete('http://localhost:3001/deleteUser/'+id)
+        .then(result => {console.log(result)
+            window.location.reload()
+        })
+        .catch(err => console.log(err))
+    }
   return (
     <div className='d-flex vh-100 bg-primary
     justify-content-center align-items-center'>
        <div className='w-50 bg-white rounded p-3'>
+            <Link to="/create" className="btn btn-success">Add +</Link>
             <table className='table'>
                 <thead>
                     <tr>
@@ -22,10 +37,15 @@ const Users = () => {
                         users.map((user) => {
                             return (
                                 <tr>
-                                    <td>{user.Name}</td>
-                                    <td>{user.Email}</td>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
                                     <td>{user.age}</td>
-                                    <td><button>Edit</button><button>Delete</button></td>
+                                    <td>
+                                    <Link to={`/update/${user._id}`} className="btn btn-success">Update</Link>
+                                        <button 
+                                            onClick={(e) => handleDelete(user._id)}
+                                            className="btn btn-danger">Delete</button>
+                                    </td>
                                 </tr>
                             ) 
                         })
